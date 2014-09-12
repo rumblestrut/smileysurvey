16559ed2b0fb6ad02b23ba73803094de05359c30
Eric Gruber <ejgruber@olatheks.org>
2014-09-12 15:35:38 -0500

    Add project files to repo

---
+++ smileysurvey.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	// Selects the How was your visit faces along with the radio button
+	$("div#edit-submitted-rate-your-visit-rate-your-visit-options-1-wrapper").click(function() {
+		$("#edit-submitted-rate-your-visit-rate-your-visit-options-1").click();
+	});
+
+	$("div#edit-submitted-rate-your-visit-rate-your-visit-options-2-wrapper").click(function() {
+		$("#edit-submitted-rate-your-visit-rate-your-visit-options-2").click();
+	});
+
+	$("div#edit-submitted-rate-your-visit-rate-your-visit-options-3-wrapper").click(function() {
+		$("#edit-submitted-rate-your-visit-rate-your-visit-options-3").click();
+	});
+});
+
